727359a61d43115552b30d54b187e6facc24359c
Ian Zhang <ianzhang1986@gmail.com>
2015-12-16 04:19:44 -0800

    [Feature] Bean loop back testing

--- Bean/SandBoxLock/SandBoxLock.ino
+++ Bean/SandBoxLock/SandBoxLock.ino
@@ -5,6 +5,8 @@
  * Input: TouchPad/BLE Verification
  * Drive: Servo
  * 
+ * Difficult to connect
+ * 
  * PWMA: 1 Attached Servo PWM Pin
  * AIN1: 0
  * AIN2: 2
@@ -13,35 +15,59 @@
  * Switch Pin Connection: (Active Low)
  * SW1: 5
  * 
- * Swithc High = Locked
- * Switch Low = Unlocked
+ * High = Locked
+ * Low = Unlocked
  */
 #include <Servo.h>
 Servo boltDrive;
 /*----------------------------------------------------*/
 #define UNLOCK_TIMEOUT_MS 300
 #define LOCK_TIMEOUT_MS   275
+#define KEYCODE_SIZE      sizeof(KEY_CODE)
 
 /* Pin Definition ------------------------------------*/
 /* TODO: check ATMEGA328p pinout */
 int PWMA = 1; // Speed Control
-int AIN1 = 0; // Direction
-int AIN2 = 2; // Direction
-int STBY = 3; // standby
+//int AIN1 = 0; // Direction
+//int AIN2 = 2; // Direction
+//int STBY = 3; // standby
 
-/* Electric Strike */
+/* Electric Strike ------------------------------------*/
 int SW1 = 5;
 
-/* Unlock keycode (hardcoded) */
-const char* KEY_CODE = "123456789";
+/* Unlock keycode ------------------------------------*/
+const char* KEY_CODE = "12345678";
 
 void setup() {
   // put your setup code here, to run once:
   boltDrive.attach(PWMA);
-  Serial.begin(57600);
+  Serial.begin(57600);      // Baud Rate
+  Serial.setTimeout(25);
+
+  // Motor Controller Setup
+  pinMode(PWMA, OUTPUT);
+//  pinMode(STBY, OUTPUT);
+//  pinMode(AIN1, OUTPUT);
+//  pinMode(AIN2, OUTPUT);
+
+  pinMode(SW1, INPUT);
+
+  Bean.enableWakeOnConnect( true ); // SDK issue? after first handshake no state change?
 }
 
 void loop() {
+  // Heartbeat
+//  Bean.setLed(0, 255, 0); 
+  
   // put your main code here, to run repeatedly:
+  char buffer[10];
+  size_t readLength = 10;
+  static char last_value = 0;
+  static char index = 0;
+  static char lock_state = 0;
 
+  readLength = Serial.readBytes(buffer, readLength);
+  Serial.print("Receive: ");
+  Serial.println(buffer);
+  Bean.sleep(0xFFFFFFFF);
 }
