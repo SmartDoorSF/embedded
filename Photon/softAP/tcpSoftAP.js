86d72ac8de5b3dea3c1e07d9495f08d7de997244
Ian Zhang <ianzhang1986@gmail.com>
2016-04-10 14:30:42 -0700

    [Cleanup] Remove softAP server connection test

--- Photon/softAP/tcpSoftAP.js
+++
@@ -1,8 +0,0 @@
-// Default softAP mode is TCP without browser
-var sap = new require('softap-setup')();
-
-sap.deviceInfo(callback);
-function callback(err, dat) {
-  if (err) { throw err; }
-  console.log("Device ID: %s, claimed: %s", dat.id, dat.claimed ? "yes" : "no");
-};
